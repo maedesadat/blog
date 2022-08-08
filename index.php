2b3d32084e07f907995beb3e76dd709bd99e4454
Maede <maedesadatmm@gmail.com>
2022-08-08 12:07:01 +0430

    init project

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="fa" dir="rtl">
+
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+    <link href="//db.onlinewebfonts.com/c/e2b31b59d40719dfa19d27a19e829da8?family=Vazir" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Blog webprog</title>
+</head>
+
+<body>
+
+
+</body>
+</html>
